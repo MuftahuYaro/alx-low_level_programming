644eeb8d8c68ba5dcaea8fab0d9b99361211b4f7
Muftahu <muftahubalarabeyaro@gmail.com>
2023-02-20 17:30:17 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i + '0');
+	putchar('\n');
+
+	return (0);
+}
