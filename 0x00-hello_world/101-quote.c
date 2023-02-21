5bbbdebf752465dbcd9ee32c4e530cc2f6748586
Muftahu <muftahubalarabeyaro@gmail.com>
2023-02-20 16:30:18 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - Prints out the lastpart of a qoute in the standard error.
  *
- * Return: 1 if successe.
+ * Return: 1 if success.
  */
 
 int main(void)
 {
-	write(2, " and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
