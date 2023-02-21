bf782d4225bd8da94b3c4cd84c1544e16b0ccd28
Muftahu <muftahubalarabeyaro@gmail.com>
2023-02-20 16:26:10 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Prints out the lastpart of a qoute in the standard error.
+ *
+ * Return: 1 if successe.
+ */
+
+int main(void)
+{
+	write(2, " and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
